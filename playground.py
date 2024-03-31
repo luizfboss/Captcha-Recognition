728f4dbb6668d51971b49c02937cbe576b938fe4
luizfboss <88173271+luizfboss@users.noreply.github.com>
2024-03-30 20:31:12 -0500

    trying to get the character selection to work

--- playground.py
+++ playground.py
@@ -69,29 +69,3 @@ import cv2
 
 # image = tensor_to_image(image)
 
-import cv2
-import easyocr
-import matplotlib.pyplot as plt
-
-# This needs to run only once to load the model into memory
-reader = easyocr.Reader(['en'])
-
-# reading the image
-img = cv2.imread('four_cap_36/0A0I-31668.png')
-
-# run OCR
-results = reader.readtext(img)
-print(results)
-
-# show the image and plot the results
-# plt.imshow(img)
-# for res in results:
-#     # bbox coordinates of the detected text
-#     xy = res[0]
-#     xy1, xy2, xy3, xy4 = xy[0], xy[1], xy[2], xy[3]
-#     # text results and confidence of detection
-#     det, conf = res[1], res[2]
-#     # show time :)
-#     plt.plot([xy1[0], xy2[0], xy3[0], xy4[0], xy1[0]], [xy1[1], xy2[1], xy3[1], xy4[1], xy1[1]], 'r-')
-#     plt.text(xy1[0], xy1[1], f'{det} [{round(conf, 2)}]')
-# 
